4fa96cd2e9065070adeeb2475488713bc7cc2a0a
alexrodriguez <alexcarvajal2404@gmail.com>
2024-05-14 15:31:45 +0200

    cambios de estilos para metodos de pago settings

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,8 +1,10 @@
 import {useQuery} from "react-query";
 import {getUserSettingsByUserId} from "@/api/user-settings.ts";
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert.tsx";
-import {AlertCircle} from "lucide-react";
+import {AlertCircle, Pencil, Trash2} from "lucide-react";
 import {useAuth} from "@clerk/clerk-react";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 export default function SettingsPage() {
     const { userId } = useAuth();
@@ -26,7 +28,29 @@ export default function SettingsPage() {
 
     return  (
         <div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <h5 className='text-2xl mb-4 font-bold'>Payment methods</h5>
+            <div className='grid grid-cols-4 gap-4'>
+                {
+                    data?.paymentMethods.map(paymentMethod => (
+                        <Card key={paymentMethod._id}>
+                            <CardHeader>
+                                <CardTitle>{paymentMethod.title}</CardTitle>
+                                {/*<CardDescription>Deploy your new project in one-click.</CardDescription>*/}
+                            </CardHeader>
+                            <CardContent>
+                            </CardContent>
+                            <CardFooter className="flex justify-end gap-3">
+                                <Button variant="outline" size='sm'>
+                                    <Pencil className='h-4 w-4'/>
+                                </Button>
+                                <Button variant="outline" size="sm">
+                                    <Trash2 className="h-4 w-4"/>
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    ))
+                }
+            </div>
         </div>
     )
 }
